ad911afbe004d5b94b8c0006718d3be41a65a8a5
Qian Yang <qianyangjy@163.com>
2019-01-30 19:11:00 +0800

    表格变成五列

--- generate.py
+++ generate.py
@@ -10,12 +10,12 @@ def main():
         item_line.append("[{0}](http://www.douyu.com/{1})".format(item[0], item[1]))
     with open("README.md", "w", encoding="utf-8") as f:
         f.write("# 斗鱼粉丝牌不完全大全（持续更新中）\n\n")
-        f.write("|1  |2  |3  |4  |5  |6  |\n")
-        f.write("|---|---|---|---|---|---|\n")
+        f.write("|1  |2  |3  |4  |5  |\n")
+        f.write("|---|---|---|---|---|\n")
         item_count = len(item_line)
         for index in range(item_count):
             f.write("| {0} ".format(item_line[index]))
-            if index % 6 == 5 or index == item_count - 1:
+            if index % 5 == 4 or index == item_count - 1:
                 f.write("|\n")
 
 
