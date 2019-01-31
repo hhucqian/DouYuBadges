618335ba5d4f269cb2ebbe5d6fec42b021351d3c
Qian Yang <qianyangjy@163.com>
2019-01-31 11:57:09 +0800

    显示当前粉丝牌总数

--- generate.py
+++ generate.py
@@ -6,10 +6,12 @@ def main():
     conn = sqlite3.connect("data.db")
     c = conn.execute("select name , room_id from badges order by name")
     item_line = []
-    for item in c.fetchall():
+    records = c.fetchall()
+    for item in records:
         item_line.append("[{0}](http://www.douyu.com/{1})".format(item[0], item[1]))
     with open("README.md", "w", encoding="utf-8") as f:
         f.write("# 斗鱼粉丝牌不完全大全（持续更新中）\n\n")
+        f.write("## 当前收录粉丝牌总数{0} \n\n".format(len(records)))
         f.write("|1  |2  |3  |4  |5  |\n")
         f.write("|---|---|---|---|---|\n")
         item_count = len(item_line)
