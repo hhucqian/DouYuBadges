d08472017902cfa26e9fb0defd82cb15acb6ff09
QianYang <qianyangjy@163.com>
2019-04-14 10:39:44 +0800

    解决数据重复插入的问题

--- badgeserver.py
+++ badgeserver.py
@@ -7,7 +7,7 @@ import threading
 class BadgeServer:
     def __init__(self):
         self.conn = sqlite3.connect('data.db', check_same_thread=False)
-        self.badge_dict = {}
+        self.badge_room_id_set = {}
         self.load_from_db()
         self.lock = threading.Lock()
 
@@ -15,14 +15,19 @@ class BadgeServer:
         c = self.conn.cursor()
         c.execute("select name, room_id from badges")
         for item in c.fetchall():
-            self.badge_dict[item[0]] = item[1]
+            self.badge_room_id_set[item[1]] = item[0]
 
     def add_badge(self, name, room_id):
+        room_id = int(room_id)
         with self.lock:
-            if name not in self.badge_dict:
-                self.badge_dict[name] = room_id
+            if room_id not in self.badge_room_id_set:
+                self.badge_room_id_set[room_id] = name
                 self.insert_badge_to_db(name, room_id)
                 return True
+            elif name != self.badge_room_id_set[room_id]:
+                self.badge_room_id_set[room_id] = name
+                self.update_badge_to_db(name, room_id)
+                return False
             return False
 
     def insert_badge_to_db(self, name, room_id):
@@ -31,10 +36,13 @@ class BadgeServer:
             "insert into badges(name, room_id) values(?, ?)", (name, room_id))
         self.conn.commit()
 
+    def update_badge_to_db(self, name, room_id):
+        print("update badge {0}-->{1}".format(name, room_id))
+        self.conn.execute(
+            "update badges set name = ? where room_id = ?", (name, room_id))
+        self.conn.commit()
+
     def get_record_count(self):
         with self.lock:
             c = self.conn.execute("select count(name) from badges;")
             return c.fetchone()[0]
-
-    def __str__(self):
-        return str(self.badge_dict)
--- generate.py
+++ generate.py
@@ -2,21 +2,15 @@
 
 import sqlite3
 
-def clear_multi(conn : sqlite3.Connection):
-    c = conn.execute("select room_id from badges group by room_id HAVING count(room_id) > 1")
-    for item in c.fetchall():
-        item1 = conn.execute("select max(rowid) from badges where room_id = ?", item).fetchone()
-        conn.execute("delete from badges where room_id = ? and rowid < ?", (item[0], item1[0]))
-    conn.commit()
 
 def main():
     conn = sqlite3.connect("data.db")
-    clear_multi(conn)
     c = conn.execute("select name , room_id from badges order by name")
     item_line = []
     records = c.fetchall()
     for item in records:
-        item_line.append("[{0}](http://www.douyu.com/{1})".format(item[0], item[1]))
+        item_line.append(
+            "[{0}](http://www.douyu.com/{1})".format(item[0], item[1]))
     with open("docs/README.md", "w", encoding="utf-8") as f:
         f.write("# 斗鱼粉丝牌不完全大全（持续更新中）\n\n")
         f.write("## 当前收录粉丝牌总数{0} \n\n".format(len(records)))
